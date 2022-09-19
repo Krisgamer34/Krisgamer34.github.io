d722c8282cc756ec358dce688b3ffcf426d9c150
Kristian <krisgamer@live.com>
2022-09-19 14:41:50 +0200

    Confus

--- Opgaver/Andengradsligningloop.py
+++ Opgaver/Andengradsligningloop.py
@@ -5,3 +5,13 @@ while x <= 100:
     print("y="+ str(y))
     print("x="+ str(x))
     x= x + 10
+    #Jeg er meget forvirret hvad du mener. 
+    #Vil du have at den tager x til 100 og så stiger med 10 hver gang 
+    #eller skal man bare sætte ekstra 0 på hver x værdi
+    x = 0
+while x <= 10:
+    y = 3*x**2 + 6*x + 9 
+
+    print("y="+ str(y))
+    print("x="+ str(x)+"0")
+    x= x + 1
