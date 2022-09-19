4c8c7c1fc2526c4ff9bc01970c744f1241950047
Kristian <krisgamer@live.com>
2022-09-19 14:42:27 +0200

    Anger

--- Opgaver/Andengradsligningloop.py
+++ Opgaver/Andengradsligningloop.py
@@ -15,3 +15,11 @@ while x <= 10:
     print("y="+ str(y))
     print("x="+ str(x)+"0")
     x= x + 1
+
+x = 0
+while x <= 10:
+    y = 3*x**2 + 6*x + 9 
+
+    print("y="+ str(y))
+    print("x="+ str(x))
+    x= x + 1
