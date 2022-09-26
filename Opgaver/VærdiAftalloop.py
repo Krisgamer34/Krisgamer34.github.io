25c3ba16544632f1331adeb3e1360f2668b6a4ae
Kristian <krisgamer@live.com>
2022-09-26 13:33:33 +0200

    dddd

--- Opgaver/VærdiAftalloop.py
+++ Opgaver/VærdiAftalloop.py
@@ -1,7 +1,18 @@
 tal = 0
 antalLoops = 0
+
 while(tal <210):
     tal= tal + antalLoops
     
     print(tal)
     antalLoops = antalLoops + 1
+
+
+def yaaaas():
+    number = 1
+    for i in range(1,10000):
+        number += i
+    print(i)
+
+
+yaaaas()
---
+++ python arbejdsmappe/Thonny demo.py
@@ -0,0 +1,3 @@
+print('goddag')
+besked = 'goddag'
+print(besked)
